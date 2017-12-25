7981fc87ea4e2cc5226c4f00dd870c37d08cdf2a
Ryohei Ueda <garaemon@gmail.com>
2017-12-25 15:09:34 +0900

    Add namespace example

---
+++ 04_namespace/src/namespace.ts
@@ -0,0 +1,9 @@
+namespace MyNameSpace {
+    export class MyClass1 {}
+    export class MyClass2 {}
+    class MyClass3 {}
+}
+
+let myClass1 = new MyNameSpace.MyClass1();
+let myClass2 = new MyNameSpace.MyClass2();
+// let myClass3 = new MyNameSpace.MyClass3(); // this is error
